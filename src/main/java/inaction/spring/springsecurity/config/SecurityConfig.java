fd34e1132d257293dd1d6318f6694bfbe9b1793c
PetterBT <petter.bergstrom@iths.se>
2022-04-11 17:26:06 +0200

    Add UserDetailsService-bean to SecurityConfig.java

--- src/main/java/inaction/spring/springsecurity/config/SecurityConfig.java
+++ src/main/java/inaction/spring/springsecurity/config/SecurityConfig.java
@@ -2,13 +2,34 @@ package inaction.spring.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        List<UserDetails> usersList = new ArrayList<>();
+
+        usersList.add(new User("ana", encoder.encode("ana"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+        usersList.add(new User("petter", encoder.encode("petter"), Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"))));
+
+        return new InMemoryUserDetailsManager(usersList);
     }
 }
