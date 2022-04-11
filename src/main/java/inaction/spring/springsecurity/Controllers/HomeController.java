67ca4824f9754cabcc966ed381e9db6c53d139e6
PetterBT <petter.bergstrom@iths.se>
2022-04-11 19:01:21 +0200

    Add files & connect to data source

---
+++ src/main/java/inaction/spring/springsecurity/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package inaction.spring.springsecurity.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
